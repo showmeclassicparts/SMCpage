49858c58c45863852be5b7a2e3fbc23d7954067a
showmeclassicparts <43041266+showmeclassicparts@users.noreply.github.com>
2018-10-17 12:19:15 -0500

    Add files via upload

--- logic.js
+++ logic.js
@@ -150,3 +150,40 @@ document.getElementById("mainclose").style.display = "none";
 // {
 //     document.getElementsByClassName('detailimg').style.height=right;
 // }
+
+// remove hover
+function watchForHover() {
+    var hasHoverClass = false;
+    var container = document.body;
+    var lastTouchTime = 0;
+
+    function enableHover() {
+        // filter emulated events coming from touch events
+        if (new Date() - lastTouchTime < 500) return;
+        if (hasHoverClass) return;
+
+        container.className += ' hasHover';
+        hasHoverClass = true;
+    }
+
+    function disableHover() {
+        if (!hasHoverClass) return;
+
+        container.className = container.className.replace(' hasHover', '');
+        hasHoverClass = false;
+    }
+
+    function updateLastTouchTime() {
+        lastTouchTime = new Date();
+    }
+
+    document.addEventListener('touchstart', updateLastTouchTime, true);
+    document.addEventListener('touchstart', disableHover, true);
+    document.addEventListener('mousemove', enableHover, true);
+
+    enableHover();
+}
+
+watchForHover();
+// remove hover end
+
