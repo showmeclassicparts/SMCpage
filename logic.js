5307abf24d51f6503f4b65a1a989e0214895339d
showmeclassicparts <43041266+showmeclassicparts@users.noreply.github.com>
2018-10-19 16:13:11 -0500

    Add files via upload

--- logic.js
+++ logic.js
@@ -365,3 +365,37 @@ TweenMax.set('#circlePath', {
       document.getElementById("loading").style.display = "none";
     }  
 // drift end
+
+
+
+//percent loader
+loader();
+
+document.querySelector('.btn').onclick = loader;
+
+/*====================================
+*     LOADER
+======================================*/
+function loader(_success) {
+    var obj = document.querySelector('.preloader'),
+    inner = document.querySelector('.preloader_inner'),
+    page = document.querySelector('.page');
+    obj.classList.add('show');
+    page.classList.remove('show');
+    var w = 0,
+        t = setInterval(function() {
+            w = w + 1;
+            inner.textContent = w+'%';
+            if (w === 100){
+                obj.classList.remove('show');
+                page.classList.add('show');
+                clearInterval(t);
+                w = 0;
+                if (_success){
+                    return _success();
+                }
+            }
+        }, 20);
+}
+
+//percent loader END
