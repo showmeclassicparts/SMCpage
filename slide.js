d08476f206f4db204414f7207bf0263fb47de8d4
Unknown <amazon@showmeclassicparts.com>
2018-10-31 16:44:24 -0500

    slider kinda done

---
+++ slide.js
@@ -0,0 +1,42 @@
+    $(document).on('ready', function() { 
+      $(".variable").slick({
+        
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        // prevArrow:true,
+        // slidesToScroll: 1,
+        swipeToSlide:true,
+        autoplay: true,
+        autoplaySpeed: 20000, 
+        // arrows: false,
+        responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            // slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            // slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            // slidesToScroll: 1
+            
+          }
+        }
+      ]
+      });
+      
+    });
